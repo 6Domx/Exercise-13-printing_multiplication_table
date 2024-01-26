6994cd7e38f8f3d86cd2a3a9dc2aa53888798faa
unknown <miggy.payumo26@gmail.com>
2024-01-26 23:58:14 +0800

    Code finalized and labeled

--- Exercise-13-printing_multiplication_table.py
+++ Exercise-13-printing_multiplication_table.py
@@ -2,7 +2,11 @@
 # 1. Create a multiplication table up to 10
 # 2. Return the result
 
+# loop for numbers across at the top
 for number_across in range(1, 11):
+# loop for numbers going down at the side
     for number_down in range(1, 11):
+# printing the result of their multiplication
         print(number_across * number_down, end= " ")
+# pattern of printing for the results
     print("\t\t")
